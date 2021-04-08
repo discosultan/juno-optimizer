009962b413da11eb6c75eaee0865b57ed83670b1
discosultan <jaanusvarus@gmail.com>
2021-04-08 14:31:50 +0200

    Init

---
+++ juno/src/stop_loss/noop.rs
@@ -0,0 +1,17 @@
+use super::StopLoss;
+use crate::genetics::Chromosome;
+use juno_derive::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Chromosome, Clone, Copy, Debug, Deserialize, Serialize)]
+pub struct NoopParams {}
+
+pub struct Noop {}
+
+impl Noop {
+    pub fn new(_params: &NoopParams) -> Self {
+        Self {}
+    }
+}
+
+impl StopLoss for Noop {}
---
+++ juno/src/take_profit/noop.rs
@@ -0,0 +1,17 @@
+use super::TakeProfit;
+use crate::genetics::Chromosome;
+use juno_derive::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Chromosome, Clone, Copy, Debug, Deserialize, Serialize)]
+pub struct NoopParams {}
+
+pub struct Noop {}
+
+impl Noop {
+    pub fn new(_params: &NoopParams) -> Self {
+        Self {}
+    }
+}
+
+impl TakeProfit for Noop {}
